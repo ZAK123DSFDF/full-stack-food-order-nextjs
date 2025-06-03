187d219fb5aa6ebab0ee0a2f3bdaa623eed6a39f
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2025-06-03 11:59:07 +0300

    fix: correct return statement in getAllMenus function

--- src/app/actions/menu/getAllMenus.ts
+++ src/app/actions/menu/getAllMenus.ts
@@ -1,13 +1,13 @@
 "use server";
 
-import { prisma } from "@/lib/prisma.ts";
+import { prisma } from "../../../lib/prisma.ts";
 
 export const getAllMenus = async () => {
   try {
-
-    return await prisma.menu.findMany({
+    const menus = await prisma.menu.findMany({
       include: { restaurant: true },
     });
+    return menus;
   } catch (error) {
     throw new Error("error in fetching menus");
   }
